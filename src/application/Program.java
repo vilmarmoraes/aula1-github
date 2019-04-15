ade3a997d00eccc3e1b4ab1398e5f6e5696b6dd4
vimarmoraes <vimarpomifrai@hotmail.com>
2019-04-14 21:31:04 -0300

    Acrescentada mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 public class Program {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bom dia!");
     }
     
 }
